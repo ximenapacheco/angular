46ea23bd5a60653037370613c9880aa6dfeb3433
Ximena <ximenacarmen1998@gmail.com>
2021-01-29 12:22:13 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab2';
+  public autor = "Ximena y Ángel";
+  public mostrar_lista = true;
+  public lista_de_usuarios = [ 'Ximena', 'Pacheco', 'Ángel', 'Tello'];
+
+  public texto_boton = "Ocultar lista";  
+
+  change():void{
+    this.mostrar_lista = !this.mostrar_lista;
+
+    if(this.mostrar_lista == false){
+      this.texto_boton = "Mostrar lista";
+    }else{
+      this.texto_boton = "Ocultar lista";
+    }
+  }
+
+  buscar() { 
+    var hay_coincidencias = false;
+    for(var i=0; i<this.lista_de_usuarios.length; i++){
+      if(this.lista_de_usuarios[i] == this.autor){
+        hay_coincidencias = true;
+      }
+    }
+    if(hay_coincidencias==true){
+      alert('Si hay coincidencias con la búsqueda');
+    }else{
+      alert('No hay coincidencias con la búsqueda');
+    }
+  }
+
 }
